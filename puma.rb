5a5f3441f7e4f6d3312d6124f890c7b269224958
Andrew Liu <andcliu@Andrews-Air-2.attlocal.net>
2024-11-14 16:02:13 -0800

    update

---
+++ puma.rb
@@ -0,0 +1,7 @@
+# config/puma.rb
+port ENV.fetch("PORT") { 4567 }
+environment ENV.fetch("RACK_ENV") { "development" }
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads threads_count, threads_count
+preload_app!
